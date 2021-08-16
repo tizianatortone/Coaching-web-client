0315e75a511a6b36e405223350159c4575d1af9f
tizianatortone <tiziana_tortone@hotmail.com>
2021-08-16 17:48:24 +0200

    Javascript works on editor but not on deploy

--- src/script.js
+++ src/script.js
@@ -1,21 +1,3 @@
-function dollarPrice() {
-  document.getElementById("price1").innerHTML = "40 USD";
-  document.getElementById("price2").innerHTML = "90 USD";
-  document.getElementById("price3").innerHTML = "150 USD";
-  document.getElementById("price4").innerHTML = "250 USD";
-  document.getElementById("currencyLink2").innerHTML = "EUR";
-  document.getElementById("currencyLink1").innerHTML = null;
-}
-
-function euroPrice() {
-  document.getElementById("price1").innerHTML = "35 EUR";
-  document.getElementById("price2").innerHTML = "80 EUR";
-  document.getElementById("price3").innerHTML = "120 EUR";
-  document.getElementById("price4").innerHTML = "220 EUR";
-  document.getElementById("currencyLink1").innerHTML = "USD";
-  document.getElementById("currencyLink2").innerHTML = null;
-}
-
 window.addEventListener("scroll", function () {
   let header = document.querySelector("header");
   let windowPosition = window.scrollY > 0;
